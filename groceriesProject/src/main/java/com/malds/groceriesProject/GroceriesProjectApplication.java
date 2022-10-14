2773b8434f689423377a7b3128a519b735866237
Lily Sam <lilyoforiwaas@gmail.com>
2022-10-14 17:45:10 -0400

    skeleton

---
+++ groceriesProject/src/main/java/com/malds/groceriesProject/GroceriesProjectApplication.java
@@ -0,0 +1,13 @@
+package com.malds.groceriesProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceriesProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceriesProjectApplication.class, args);
+	}
+
+}
---
+++ groceriesProject/src/test/java/com/malds/groceriesProject/GroceriesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malds.groceriesProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceriesProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
