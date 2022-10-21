af332bd655bd99be90a7569bf166ac73a13d6c25
Sarina Sugita <sarinasugitax@gmail.com>
2022-10-21 18:56:13 -0400

    using dynaoDBMapper

--- groceriesProject/src/main/java/com/malds/groceriesProject/repositories/clientRepository.java
+++ groceriesProject/src/main/java/com/malds/groceriesProject/repositories/clientRepository.java
@@ -1,12 +1,17 @@
 package com.malds.groceriesProject.repositories;
 
-//import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Repository;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import org.springframework.data.repository.CrudRepository;
 import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 
 import com.malds.groceriesProject.models.Client;
 
-@EnableScan
-public interface clientRepository extends CrudRepository<Client, Integer>{
-
+//@EnableScan
+//public interface clientRepository extends CrudRepository<Client, Integer>{
+@Repository
+public class clientRepository{
+    @Autowired
+    private DynamoDBMapper dynamoDBMapper;
 }
