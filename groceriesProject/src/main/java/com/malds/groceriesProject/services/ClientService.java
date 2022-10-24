fb6ea62bbd3027e9829aa487f3a4bd6713f5fb34
Sarina Sugita <sarinasugitax@gmail.com>
2022-10-24 09:22:42 -0400

    Client service logic added

--- groceriesProject/src/main/java/com/malds/groceriesProject/services/ClientService.java
+++ groceriesProject/src/main/java/com/malds/groceriesProject/services/ClientService.java
@@ -1,12 +1,39 @@
 package com.malds.groceriesProject.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.malds.groceriesProject.models.Client;
 import com.malds.groceriesProject.repositories.ClientRepository;
 
 @Service
 public class ClientService {
     @Autowired
     private ClientRepository clientRepo;
+    public ClientService(ClientRepository clientRepo) {
+        this.clientRepo = clientRepo;
+    }
+
+    public Client saveClient(Client client) {
+        return clientRepo.saveClient(client);
+    }
+
+    public Client getClientByID(Integer clientID) {
+        return clientRepo.getClientByID(clientID);
+    }
+
+    public void deleteClientByID(Integer clientID) {
+        clientRepo.deleteClientByID(clientID);
+    }
+
+    public Client updateClient(Integer clientID, Client client) {
+        return clientRepo.updateClient(clientID, client);
+    }
+
+    public List<Client> findAll() {
+            return clientRepo.findAll();
+    }
+
 }
