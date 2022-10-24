9436fbe7506a27ff602373815ed63bfdec7eb5e7
Lily Sam <lilyoforiwaas@gmail.com>
2022-10-24 15:47:48 -0400

    product code to remote branch

--- groceriesProject/src/main/java/com/malds/groceriesProject/repositories/ProductRepository.java
+++ groceriesProject/src/main/java/com/malds/groceriesProject/repositories/ProductRepository.java
@@ -1,11 +1,29 @@
 package com.malds.groceriesProject.repositories;
 
-import org.springframework.stereotype.Repository;
-import org.springframework.beans.factory.annotation.Autowired;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
+import com.malds.groceriesProject.models.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
-public class ProductRepository{
+public class ProductRepository {
+
     @Autowired
     private DynamoDBMapper dynamoDBMapper;
+
+    public Product getProductById(Integer productId) {
+
+        return dynamoDBMapper.load(Product.class, productId);
+    }
+
+    public List<Product> getAllProducts(){
+
+        DynamoDBQueryExpression<Product> queryExpression = new DynamoDBQueryExpression<Product>();
+        List<Product> productList = dynamoDBMapper.query(Product.class,queryExpression);
+        return productList;
+
+    }
 }
--- groceriesProject/src/main/java/com/malds/groceriesProject/services/ProductService.java
+++ groceriesProject/src/main/java/com/malds/groceriesProject/services/ProductService.java
@@ -1,12 +1,19 @@
 package com.malds.groceriesProject.services;
 
+import com.malds.groceriesProject.models.Product;
+import com.malds.groceriesProject.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.malds.groceriesProject.repositories.ProductRepository;
-
 @Service
 public class ProductService {
+
     @Autowired
     private ProductRepository productRepository;
+
+    // find product by ID
+    public Product getProductByID(Integer productID) {
+        return null;
+    }
+
 }
