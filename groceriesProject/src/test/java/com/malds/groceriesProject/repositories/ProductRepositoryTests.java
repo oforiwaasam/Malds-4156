d247c2e9d9c57fd8571a2556e2f5b0f6014151d0
ammr-mo <ahm2169@columbia.edu>
2022-10-25 17:51:14 -0400

    Updated Product Swagger testing and dynamodb, added a few tests for product

--- groceriesProject/src/test/java/com/malds/groceriesProject/repositories/ProductRepositoryTests.java
+++ groceriesProject/src/test/java/com/malds/groceriesProject/repositories/ProductRepositoryTests.java
@@ -8,7 +8,6 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.malds.groceriesProject.models.Product;
 import com.malds.groceriesProject.services.ProductService;
 
@@ -21,35 +20,37 @@ public class ProductRepositoryTests {
     @MockBean
     private ProductRepository productRepo;
 
-    private DynamoDBMapper dynamoDBMapper;
-
 
     @Test
     public void testAddProduct(){
 
         Product product = new Product();
-        product.setProductID(123456);
-        product.setVendorID(54321);
-        product.setPrice(4.19);
-        product.setQuantity(1);
+        product.setProductID("123456");
+        product.setVendorID("54321");
+        product.setPrice("4.19");
+        product.setQuantity("1");
         
         Mockito.when(productRepo.addProduct(product)).thenReturn(product);
         
-        Assertions.assertEquals(productService.addNewProduct(product).getProductID(), 123456);
+        Assertions.assertEquals(productService.addNewProduct(product).getProductID(), "123456");
     }
 
-    // @Test
-    // public void testFindProductById() {
+    @Test
+    public void testFindProductById() {
 
-    //     Product returnProduct = new Product();
-    //     returnProduct.setProductID(123456);
-    //     returnProduct.setVendorID(54321);
-    //     returnProduct.setPrice(4.19);
-    //     returnProduct.setQuantity(1);
+        Product returnProduct = new Product();
+        returnProduct.setProductID("123456");
+        returnProduct.setVendorID("54321");
+        returnProduct.setPrice("4.19");
+        returnProduct.setQuantity("1");
 
-    //     Mockito.when(productRepo.addProduct(returnProduct)).thenReturn(returnProduct);
+        
+        Mockito.when(productRepo.findProductById("123456")).thenReturn(returnProduct);
     
-    //     productService.addNewProduct(returnProduct);
-    //     Assertions.assertEquals(productService.getProductByID(123456), returnProduct);
-    // }
+        // productService.addNewProduct(returnProduct);
+        Assertions.assertEquals(productService.getProductByID("123456"), returnProduct);
+    }
+
+
+
 }
