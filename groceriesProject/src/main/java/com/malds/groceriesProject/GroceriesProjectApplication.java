6e9b0fa2a146bfdf1932a88271e0fcc321742307
Sarina Sugita <sarinasugitax@gmail.com>
2022-10-30 15:06:51 -0400

    testing heroku compatibility

--- groceriesProject/src/main/java/com/malds/groceriesProject/GroceriesProjectApplication.java
+++ groceriesProject/src/main/java/com/malds/groceriesProject/GroceriesProjectApplication.java
@@ -2,10 +2,17 @@ package com.malds.groceriesProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class GroceriesProjectApplication {
 
+	@RequestMapping("/")
+    @ResponseBody
+    String home() {
+      return "Hello World!";
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(GroceriesProjectApplication.class, args);
 	}
