9cb67e213dc8f422ccb4acf03ea0a9ba7dbdd7af
Mark Calvario <58496944+markcalvario@users.noreply.github.com>
2022-11-21 14:10:51 -0500

    - resolved check style errors for shoppingList service, repo, controller, and model

--- groceriesProject/src/main/java/com/malds/groceriesProject/services/ShoppingListService.java
+++ groceriesProject/src/main/java/com/malds/groceriesProject/services/ShoppingListService.java
@@ -12,36 +12,77 @@ import com.malds.groceriesProject.repositories.ShoppingListRepository;
 
 @Service
 public class ShoppingListService {
+    /**
+     * ShoppingListService
+     * Carries out the operations and interacts with the persistence layer,
+     * ShoppingList Repository, to create, read, update, and
+     * delete from the database.
+     */
     @Autowired
     private ShoppingListRepository shoppingListRepository;
 
-    public ShoppingListService(ShoppingListRepository shoppingListRepository) {
+    /**
+     * ShoppingListService Constructor.
+     * @param shoppingListRepository
+     */
+    public ShoppingListService(final ShoppingListRepository
+                                       shoppingListRepository) {
         this.shoppingListRepository = shoppingListRepository;
     }
 
-    public List<ShoppingList> updateShoppingList(ShoppingList shoppingList)
+    /**
+     * Updates existing ShoppingList info and returns
+     * updated ShoppingList in a list.
+     * Throws ResourceNotFoundException if ShoppingList does not exist.
+     * @param shoppingList
+     * @return List containing the updated ShoppingList
+     * @throws ResourceNotFoundException
+     */
+    public List<ShoppingList> updateShoppingList(
+            final ShoppingList shoppingList)
             throws ResourceNotFoundException {
-        if (shoppingListRepository.getShoppingListByID(shoppingList.getShoppingListID()) != null) {
+        if (shoppingListRepository.getShoppingListByID(
+                shoppingList.getShoppingListID()) != null) {
             return shoppingListRepository.updateShoppingList(shoppingList);
         } else {
             throw new ResourceNotFoundException("Shopping List ID not found");
         }
     }
 
-    public List<ShoppingList> getShoppingListByID(String shoppingListID)
+    /**
+     * given shoppingListID input, searches the ShoppingList
+     * table in dynamoDB and returns the shoppingList with
+     * shoppingListID.
+     * Throws ResourceNotFoundException if shoppingListID does not exist.
+     * @param shoppingListID
+     * @return List containing ShoppingList with specified shoppingListID
+     * @throws ResourceNotFoundException
+     */
+    public List<ShoppingList> getShoppingListByID(final String shoppingListID)
             throws ResourceNotFoundException {
         ShoppingList shop;
         if (shoppingListRepository.retriveList(shoppingListID).size() > 0) {
             shop = shoppingListRepository.getShoppingListByID(shoppingListID);
         } else {
-            throw new ResourceNotFoundException("This shoppingList ID doesn't exists");
+            throw new ResourceNotFoundException(
+                    "This shoppingList ID doesn't exists");
         }
         return List.of(shop);
     }
 
-    public List<ShoppingList> createShoppingList(ShoppingList shoppingList) throws Exception {
+    /**
+     * Saves/creates new ShoppingList into the ShoppingList
+     * table in dynamoDB and returns the newly created ShoppingList.
+     * throws Exception if shoppingListID already exists
+     * @param shoppingList
+     * @return List containing the saved/created shoppingList
+     * @throws Exception
+     */
+    public List<ShoppingList> createShoppingList(
+            final ShoppingList shoppingList)throws Exception {
         ShoppingList shop;
-        if (shoppingListRepository.retriveList(shoppingList.getShoppingListID()).size() == 0) {
+        if (shoppingListRepository.retriveList(
+                shoppingList.getShoppingListID()).size() == 0) {
             shop = shoppingListRepository.createShoppingList(shoppingList);
         } else {
             throw new Exception("This shoppingList ID already exists");
@@ -49,35 +90,68 @@ public class ShoppingListService {
         return List.of(shop);
     }
 
-    public void deleteShoppingListByID(String shoppingListID) throws ResourceNotFoundException {
+    /**
+     * Deletes shoppingList in the ShoppingList table in dynamoDB
+     * throws ResourceNotFoundException if shoppingListID does not exist.
+     * @param shoppingListID
+     * @throws ResourceNotFoundException
+     */
+    public void deleteShoppingListByID(final String shoppingListID)
+            throws ResourceNotFoundException {
         if (shoppingListRepository.existsByID(shoppingListID)) {
             shoppingListRepository.deleteShoppingListByID(shoppingListID);
         } else {
-            throw new ResourceNotFoundException("This shoppingList ID doesn't exist");
+            throw new ResourceNotFoundException("This "
+                    + "shoppingList ID doesn't exist");
         }
     }
 
-    public Map<String, String> getProductsToQuantityByID(final String shoppingListID)
+    /**
+     * given shoppingListID input, searches the ShoppingList
+     * table in dynamoDB and returns a hashmap containing
+     * the products with their respective quantity within that ShoppingList.
+     * Throws ResourceNotFoundException if shoppingListID does not exist.
+     * @param shoppingListID
+     * @return A hashmap where the key is the productID and
+     * the value is the quantity
+     * @throws ResourceNotFoundException
+     */
+    public Map<String, String> getProductsToQuantityByID(
+            final String shoppingListID)
             throws ResourceNotFoundException {
         if (shoppingListRepository.existsByID(shoppingListID)) {
-            return shoppingListRepository.getProductsToQuantityByID(shoppingListID);
+            return shoppingListRepository.
+                    getProductsToQuantityByID(shoppingListID);
         } else {
-            throw new ResourceNotFoundException("This shoppingList ID doesn't exist");
+            throw new ResourceNotFoundException("This "
+                    + "shoppingList ID doesn't exist");
         }
     }
 
-    public void checkValidInput(ShoppingList shoppingList) throws Exception{
-        if (shoppingList.getShoppingListID() == null || shoppingList.getClientID() == null
-            || shoppingList.getProductIDToQuantity() == null) {
+    /**
+     * Checks whether inputted values by users are valid,
+     * is not blank, and is of accepted data types.
+     * Throws Exception if inputs are invalid.
+     * @param shoppingList
+     * @throws Exception
+     */
+    public void checkValidInput(
+            final ShoppingList shoppingList) throws Exception {
+        if (shoppingList.getShoppingListID() == null
+                || shoppingList.getClientID() == null
+                || shoppingList.getProductIDToQuantity() == null) {
             throw new Exception("Value cannot be null");
         }
 
-        for (Map.Entry<String, String> entry : shoppingList.getProductIDToQuantity().entrySet()) {
+        for (Map.Entry<String, String> entry : shoppingList
+                .getProductIDToQuantity().entrySet()) {
             String quantity = entry.getValue();
-            if (!(quantity.matches("[0-9]+"))){
-                throw new Exception("The quantity value within the productIDToQuantity is invalid. Make sure it only contains numbers");
+            if (!(quantity.matches("[0-9]+"))) {
+                throw new Exception("The quantity value "
+                        + "within the productIDToQuantity"
+                        + " is invalid. Make sure it only "
+                        + "contains numbers");
             }
         }
-
     }
 }
