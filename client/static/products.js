4b64846672c752e0928b9d52d978ffe57fb9d48a
Mark Calvario <58496944+markcalvario@users.noreply.github.com>
2022-11-27 05:27:18 -0500

    - vendor is able to update/delete product(s)

--- client/static/products.js
+++ client/static/products.js
@@ -2,6 +2,44 @@ const addProductBtn = document.getElementById("add-product-btn");
 const productNameInput = document.getElementById("product-name");
 const productPriceInput = document.getElementById("product-price");
 const productQuantityInput = document.getElementById("product-quantity");
+const vendorProductsDiv = document.getElementById("vendor-products")
+
+const deleteProductWithID = (productID) => {
+    $.ajax({
+        type: 'DELETE',
+        url: `http://localhost:8080/products/${productID}`,
+        contentType: 'application/json; charset=utf-8',
+        success: function(result){
+            console.log(result);
+            location.reload()
+        },
+        error: function(request, status, error){
+            console.log('Error');
+            console.log(request);
+            console.log(status);
+            console.log(error);
+        }
+    })
+}
+
+const updateProduct = (product, productID) => {
+    $.ajax({
+        type: 'PUT',
+        url: `http://localhost:8080/products/${productID}`,
+        data: JSON.stringify(product),
+        dataType: "json",
+        contentType: 'application/json; charset=utf-8',
+        success: function(result){
+            console.log(result);
+        },
+        error: function(request, status, error){
+            console.log('Error');
+            console.log(request);
+            console.log(status);
+            console.log(error);
+        }
+    })
+}
 
 const addProduct = (product) => {
     $.ajax({
@@ -15,6 +53,7 @@ const addProduct = (product) => {
             productNameInput.value = ""
             productPriceInput.value = ""
             productQuantityInput.value = ""
+            location.reload()
         },
         error: function(request, status, error){
             console.log('Error');
@@ -47,7 +86,124 @@ addProductBtn.addEventListener("click", ()=>{
 })
 
 const displayProducts = (products) =>{
+    for (let index in products) {
+        const product = products[index]
+        const productID = product['productID']
+        const productName = product['productName']
+        let productPrice = product['price'];
+        let productQuantity = product['quantity'];
 
+        const rowDiv = document.createElement("div");
+        const productNameDiv = document.createElement("div");
+        const priceDiv = document.createElement("div");
+        const quantityDiv = document.createElement("div");
+        const editBtnDiv = document.createElement("div");
+        const editBtn = document.createElement("btn");
+        const saveBtnDiv = document.createElement("div");
+        const saveBtn = document.createElement("btn");
+        const deleteBtnDiv = document.createElement("div");
+        const deleteBtn = document.createElement("btn");
+
+        productNameDiv.classList.add("col-2");
+        productNameDiv.innerHTML = productName;
+        priceDiv.classList.add("col-2")
+        priceDiv.innerHTML = productPrice;
+        quantityDiv.classList.add("col-2");
+        quantityDiv.innerHTML = productQuantity;
+
+
+        editBtn.innerHTML = "Edit"
+        editBtn.classList.add("btn", "btn-dark");
+        editBtnDiv.classList.add("col-2");
+        editBtnDiv.append(editBtn)
+
+        saveBtn.innerHTML = "Save"
+        saveBtn.classList.add("btn", "btn-primary")
+        saveBtnDiv.classList.add("col-2", "d-none");
+        saveBtnDiv.append(saveBtn)
+
+        deleteBtn.innerHTML = "Delete";
+        deleteBtn.classList.add("btn", "btn-danger")
+        deleteBtnDiv.classList.add("col-2", "d-none");
+        deleteBtnDiv.append(deleteBtn);
+
+        deleteBtn.addEventListener("click", () => {
+            deleteProductWithID(productID);
+        })
+
+        saveBtn.addEventListener("click", ()=>{
+            const priceInput = document.getElementById("price-input");
+            const quantityInput = document.getElementById("quantity-input");
+            const newPrice = priceInput.value;
+            const newQuantity = quantityInput.value;
+
+            if ( ( (newPrice != productPrice) || (newQuantity != productQuantity) )
+                && (newPrice.length != 0) && (newQuantity.length !=0) ) {
+                product['price'] = newPrice;
+                product['quantity'] = newQuantity;
+                updateProduct(product, productID);
+                priceDiv.innerHTML = newPrice;
+                quantityDiv.innerHTML = newQuantity;
+                productPrice = newPrice;
+                productQuantity = newQuantity;
+            } else {
+                priceDiv.innerHTML = productPrice;
+                quantityDiv.innerHTML = productQuantity;
+            }
+            editBtn.innerHTML = "Edit"
+            saveBtnDiv.classList.add("d-none")
+            deleteBtnDiv.classList.add("d-none")
+
+        })
+
+        editBtn.addEventListener("click", ()=>{
+            if (editBtn.innerHTML == "Cancel"){
+                priceDiv.innerHTML = productPrice;
+                quantityDiv.innerHTML = productQuantity;
+                editBtn.innerHTML = "Edit"
+                saveBtnDiv.classList.add("d-none")
+                deleteBtnDiv.classList.add("d-none")
+            } else {
+                priceDiv.innerHTML = "";
+                quantityDiv.innerHTML = "";
+                saveBtnDiv.classList.remove("d-none");
+                deleteBtnDiv.classList.remove("d-none")
+
+                const priceInput = document.createElement("input");
+                const quantityInput = document.createElement("input");
+                priceInput.id = "price-input";
+                quantityInput.id = "quantity-input";
+
+                priceInput.classList.add("form-control");
+                quantityInput.classList.add("form-control");
+
+                priceInput.placeholder = productPrice;
+                quantityInput.placeholder = productQuantity;
+
+                priceDiv.append(priceInput);
+                quantityDiv.append(quantityInput);
+
+                editBtn.innerHTML = "Cancel"
+            }
+        })
+
+
+        rowDiv.classList.add("row", "py-4", "mx-auto");
+        rowDiv.append(productNameDiv, priceDiv, quantityDiv , editBtnDiv, saveBtnDiv, deleteBtnDiv);
+        vendorProductsDiv.append(rowDiv);
+    }
 }
 
+const getProductsByVendorID = async () =>{
+    const vendorID = "2"
+    const response = await fetch(`http://localhost:8080/products/get_product_by_vendor_id/${vendorID}`);
+    const data = await response.json();
+    return data
+}
 
+const init = async () =>{
+    const products = await getProductsByVendorID();
+    displayProducts(products);
+}
+
+init();
