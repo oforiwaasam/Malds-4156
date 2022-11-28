a65627d3f968b0b03ea5131d03b5ff1bcb6891f6
ammr-mo <ahm2169@columbia.edu>
2022-11-28 07:54:01 -0500

    Auth0

---
+++ groceriesProject/src/main/java/com/malds/groceriesProject/controllers/APIController.java
@@ -0,0 +1,31 @@
+package com.malds.groceriesProject.controllers;
+
+import com.malds.groceriesProject.models.Message;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api", produces = MediaType.APPLICATION_JSON_VALUE)
+// For simplicity of this sample, allow all origins. Real applications should configure CORS for their use case.
+@CrossOrigin(origins = "*")
+public class APIController {
+
+    @GetMapping(value = "/public")
+    public Message publicEndpoint() {
+        return new Message("All good. You DO NOT need to be authenticated to call /api/public.");
+    }
+
+    @GetMapping(value = "/private/{authorization}")
+    public Message privateEndpoint(@PathVariable("authorization") final String token) {
+        return new Message("All good. You can see this because you are Authenticated.");
+    }
+
+    @GetMapping(value = "/private-scoped")
+    public Message privateScopedEndpoint() {
+        return new Message("All good. You can see this because you are Authenticated with a Token granted the 'read:messages' scope");
+    }
+}
---
+++ groceriesProject/src/main/java/com/malds/groceriesProject/models/Message.java
@@ -0,0 +1,14 @@
+package com.malds.groceriesProject.models;
+// src/main/java/com/auth0/model/Message.java
+
+public class Message {
+    private final String message;
+
+    public Message(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+}
